0374d2d39b28950acbabcc4e39b67677326893b9
Max Edell <maxed@adobe.com>
2018-04-11 16:44:05 -0400

    add 3 color sampling methods

--- collection_modules/ambiLEDCollectionPoint/ambiLEDCollectionPoint.py
+++ collection_modules/ambiLEDCollectionPoint/ambiLEDCollectionPoint.py
@@ -1,6 +1,8 @@
 import cv2
 from threading import Thread
 import time
+import numpy as np
+from scipy.stats import itemfreq
 from collectionPointEvent import CollectionPointEvent
 from threadsafeLogger import ThreadsafeLogger
 
@@ -40,11 +42,16 @@ class TVCollectionPoint(Thread):
         # Constants
         self._captureWidth = 1600
         self._captureHeight = 900
-        self._numLEDS = 60
+        self._numLEDs = 60
         self._collectionPointId = "tvcam1"
         self._collectionPointType = "ambiLED"
         self._showVideoStream = True
         self._delimiter = ';'
+        self._colorMode = 'edgeDominant'
+        # self._colorMode = 'edgeMean'
+        self._perimeterDepth = 20
+        self._topSegments = 3
+        self._sideSegments = 2
 
         # Logger
         self.logger = ThreadsafeLogger(loggingQueue, __name__)
@@ -81,9 +88,8 @@ class TVCollectionPoint(Thread):
                 cv2.imshow('Set ROI', framecopy)
                 cv2.setMouseCallback('Set ROI', self.getROI, frame)
                 k = cv2.waitKey(0)
-                if k == 27 and self.boundSet:
-                    # on esc, user wants to exit, only allow them to exit if bounds set
-                    print('k: ', k)
+                if k == 32 and self.boundSet:
+                    # on space, user wants to finalize bounds, only allow them to exit if bounds set
                     cont = False
                 # elif k != 27:
                     # any other key clears rectangles
@@ -93,18 +99,195 @@ class TVCollectionPoint(Thread):
                     # cv2.setMouseCallback('Set ROI', self.getROI, framecopy)
         cv2.destroyWindow('Set ROI')
 
+        # self.ix = 300
+        # self.fx = 600
+        # self.iy = 300
+        # self.fy = 600
+
+        self.initKMeans()
+
+        # Set up mode
+        # if self._colorMode == 'dominant':
+
+        if self._colorMode == 'edgeDominant' or self._colorMode == 'edgeMean':
+            top_length_pixels = self.fx-self.ix
+            side_length_pixels = self.fy-self.iy
+            perimeter_length_pixels = top_length_pixels*2 + side_length_pixels*2
+            print('perimeter_length_pixels: %s'%perimeter_length_pixels)
+            perimeter_depth = 0
+            if self._perimeterDepth < side_length_pixels/2 and self._perimeterDepth < top_length_pixels/2:
+                perimeter_depth = self._perimeterDepth
+            else: 
+                perimeter_depth = min(side_length_pixels/2, top_length_pixels/2)
 
         while self.alive:
-            ok, frame = self.video.read()
+            ok, ogframe = self.video.read()
             if not ok:
                 self.logger.error('Error while reading frame')
                 break
-                
+            frame = ogframe.copy()
+
+            #resize frame for color sampling
+            # frame = cv2.resize(fframe, (0,0), fx=0.5, fy=0.5)
+
+            # Dominant color
+            if self._colorMode == 'dominant':
+                dominant_color = self.getDominantColor(cv2.resize(frame[:,:,:], (0,0), fx=0.4, fy=0.4), self.ix, self.fx, self.iy, self.fy)
+                #self.putCPMessage(dominant_color, 'light-dominant')
+                print(dominant_color)
+
+            elif self._colorMode == 'edgeMean':
+                # assuming LEDs are evenly distributed, find number for each edge of ROI
+                top_num_leds = self._numLEDs*(top_length_pixels/perimeter_length_pixels)
+                side_num_leds = self._numLEDs*(side_length_pixels/perimeter_length_pixels)
+                top_segment_length = top_length_pixels/self._topSegments
+                side_segment_length = side_length_pixels/self._sideSegments
+
+                for i in range(0, self._topSegments):
+                    ix = int(self.ix+i*top_segment_length)
+                    fx = int(self.ix+(i+1)*top_segment_length)
+                    iy = int(self.iy)
+                    fy = int(self.iy+perimeter_depth)
+                    c = self.getMeanColor(cv2.resize(frame[ix:fx, iy:fy, :], (0,0), fx=0.2, fy=0.2))
+                    print('top segment %s, c: '%i, c)
+                    if self._showVideoStream:
+                        cv2.rectangle(frame, (ix, iy), (fx, fy), (0,0,255), 1)
+                        cv2.rectangle(frame, (ix, iy-(10+perimeter_depth)), (fx, fy-perimeter_depth), (int(c[0]), int(c[1]), int(c[2])), 10)
+
+                for i in range(0, self._sideSegments):
+
+                    ix = int(self.fx-perimeter_depth)
+                    fx = int(self.fx)
+                    iy = int(self.iy+i*side_segment_length)
+                    fy = int(self.iy+(i+1)*side_segment_length)
+                    c = self.getMeanColor(cv2.resize(frame[ix:fx, iy:fy, :], (0,0), fx=0.2, fy=0.2))
+                    print('right segment %s, c: '%i, c)
+                    if self._showVideoStream:
+                        cv2.rectangle(frame, (ix, iy), (fx, fy), (0,255,0), 1)
+                        cv2.rectangle(frame, (ix+perimeter_depth, iy), (fx+(10+perimeter_depth), fy), (int(c[0]), int(c[1]), int(c[2])), 10)
+
+                for i in range(0, self._topSegments):
+
+                    ix = int(self.fx-(i+1)*top_segment_length) 
+                    fx = int(self.fx-i*top_segment_length)
+                    iy = int(self.fy-perimeter_depth)
+                    fy = int(self.fy)
+                    c = self.getMeanColor(cv2.resize(frame[ix:fx, iy:fy, :], (0,0), fx=0.2, fy=0.2))
+                    print('bottom segment %s, c: '%i, c)
+                    if self._showVideoStream:
+                        cv2.rectangle(frame, (ix, iy), (fx, fy), (0,0,255), 1)
+                        cv2.rectangle(frame, (ix, iy+perimeter_depth), (fx, fy+(10+perimeter_depth)), (int(c[0]), int(c[1]), int(c[2])), 10)
+
+                for i in range(0, self._sideSegments):
+
+                    ix = int(self.ix)
+                    fx = int(self.ix+perimeter_depth)
+                    iy = int(self.fy-(i+1)*side_segment_length)
+                    fy = int(self.fy-i*side_segment_length)
+                    c = self.getMeanColor(cv2.resize(frame[ix:fx, iy:fy, :], (0,0), fx=0.2, fy=0.2))
+                    print('left segment %s, c: '%i, c)
+                    if self._showVideoStream:
+                        cv2.rectangle(frame, (ix, iy), (fx, fy), (0,255,0), 1)
+                        cv2.rectangle(frame, (ix-(10+perimeter_depth), iy), (fx-perimeter_depth, fy), (int(c[0]), int(c[1]), int(c[2])), 10)
+
+            elif self._colorMode == 'edgeDominant':
+                # assuming LEDs are evenly distributed, find number for each edge of ROI
+                top_num_leds = self._numLEDs*(top_length_pixels/perimeter_length_pixels)
+                side_num_leds = self._numLEDs*(side_length_pixels/perimeter_length_pixels)
+                top_segment_length = top_length_pixels/self._topSegments
+                side_segment_length = side_length_pixels/self._sideSegments
+
+                for i in range(0, self._topSegments):
+                    ix = int(self.ix+i*top_segment_length)
+                    fx = int(self.ix+(i+1)*top_segment_length)
+                    iy = int(self.iy)
+                    fy = int(self.iy+perimeter_depth)
+                    c = self.getDominantSegmentColor(cv2.resize(frame[ix:fx, iy:fy, :], (0,0), fx=0.2, fy=0.2))
+                    if self._showVideoStream:
+                        cv2.rectangle(frame, (ix, iy), (fx, fy), (0,0,255), 1)
+                        cv2.rectangle(frame, (ix, iy-(10+perimeter_depth)), (fx, fy-perimeter_depth), (int(c[0]), int(c[1]), int(c[2])), 10)
+
+                for i in range(0, self._sideSegments):
+                    ix = int(self.fx-perimeter_depth)
+                    fx = int(self.fx)
+                    iy = int(self.iy+i*side_segment_length)
+                    fy = int(self.iy+(i+1)*side_segment_length)
+                    ff = cv2.resize(frame[ix:fx, iy:fy, :], (0,0), fx=0.2, fy=0.2)
+                    c = self.getDominantSegmentColor(ff)
+                    if self._showVideoStream:
+                        cv2.rectangle(frame, (ix, iy), (fx, fy), (0,255,0), 1)
+                        cv2.rectangle(frame, (ix+perimeter_depth, iy), (fx+(10+perimeter_depth), fy), (int(c[0]), int(c[1]), int(c[2])), 10)
+
+                for i in range(0, self._topSegments):
+                    ix = int(self.fx-(i+1)*top_segment_length) 
+                    fx = int(self.fx-i*top_segment_length)
+                    iy = int(self.fy-perimeter_depth)
+                    fy = int(self.fy)
+                    c = self.getDominantSegmentColor(cv2.resize(frame[ix:fx, iy:fy, :], (0,0), fx=0.2, fy=0.2))
+                    if self._showVideoStream:
+                        cv2.rectangle(frame, (ix, iy), (fx, fy), (0,0,255), 1)
+                        cv2.rectangle(frame, (ix, iy+perimeter_depth), (fx, fy+(10+perimeter_depth)), (int(c[0]), int(c[1]), int(c[2])), 10)
+
+                for i in range(0, self._sideSegments):
+                    ix = int(self.ix)
+                    fx = int(self.ix+perimeter_depth)
+                    iy = int(self.fy-(i+1)*side_segment_length)
+                    fy = int(self.fy-i*side_segment_length)
+                    c = self.getDominantSegmentColor(cv2.resize(frame[ix:fx, iy:fy, :], (0,0), fx=0.2, fy=0.2))
+                    if self._showVideoStream:
+                        cv2.rectangle(frame, (ix, iy), (fx, fy), (0,255,0), 1)
+                        cv2.rectangle(frame, (ix-(10+perimeter_depth), iy), (fx-perimeter_depth, fy), (int(c[0]), int(c[1]), int(c[2])), 10)
+
             if self._showVideoStream:
                 cv2.rectangle(frame, (self.ix, self.iy), (self.fx, self.fy), (255,0,0), 1)
                 cv2.imshow("output", frame)
                 cv2.waitKey(1)
 
+    def getMeanColor(self, frame):
+        color = [frame[:,:,i].mean() for i in range(frame.shape[-1])]
+        return color
+
+    def initKMeans(self):
+         # kmeans vars
+        self.n_colors = 5
+        self.criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
+        self.flags = cv2.KMEANS_RANDOM_CENTERS
+
+    def getDominantSegmentColor(self, segment):
+        average_color = [segment[:,:,i].mean() for i in range(segment.shape[-1])]
+        arr = np.float32(segment)
+        pixels = arr.reshape((-1, 3))
+
+        # kmeans clustering
+        _, labels, centroids = cv2.kmeans(pixels, self.n_colors, None, self.criteria, 10, self.flags)
+
+        palette = np.uint8(centroids)
+        quantized = palette[labels.flatten()]
+        quantized = quantized.reshape(segment.shape)
+
+        dominant_color = palette[np.argmax(itemfreq(labels)[:, -1])]
+
+        return dominant_color
+
+    def getDominantColor(self, img, ix, fx, iy, fy):
+        ix = int(ix)
+        fx = int(fx)
+        iy = int(iy)
+        fy = int(fy)
+        average_color = [img[ix:fx, iy:fy, i].mean() for i in range(img.shape[-1])]
+        arr = np.float32(img)
+        pixels = arr.reshape((-1, 3))
+
+        # kmeans clustering
+        _, labels, centroids = cv2.kmeans(pixels, self.n_colors, None, self.criteria, 10, self.flags)
+
+        palette = np.uint8(centroids)
+        quantized = palette[labels.flatten()]
+        quantized = quantized.reshape(img.shape)
+
+        dominant_color = palette[np.argmax(itemfreq(labels)[:, -1])]
+
+        return dominant_color
 
     def initializeCamera(self):
         # open first webcam available
@@ -168,7 +351,7 @@ class TVCollectionPoint(Thread):
                 None)
             self.outQueue.put(msg)
 
-        elif type == "light":
+        elif type == "light-edges":
             # Reset collection start and now needs needs reset
             collectionStart = time.time()
 
@@ -176,7 +359,20 @@ class TVCollectionPoint(Thread):
             msg = CollectionPointEvent(
                 self._collectionPointId,
                 self._collectionPointType,
-                'light',
+                'light-edges',
+                data
+            )
+            self.outQueue.put(msg)
+
+        elif type == "light-dominant":
+            # Reset collection start and now needs needs reset
+            collectionStart = time.time()
+
+            self.logger.info('Sending light message')
+            msg = CollectionPointEvent(
+                self._collectionPointId,
+                self._collectionPointType,
+                'light-dominant',
                 data
             )
             self.outQueue.put(msg)
